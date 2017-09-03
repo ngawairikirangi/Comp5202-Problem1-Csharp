9b0415d6453ed343837ce66e8dfedae41694a457
Ngawai Rikirangi <9978212@BCS.NET.NZ>
2017-09-04 11:32:54 +1200

    problem1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace Comp5202_Problem1_Csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ask the user for a number input (for loop)//
+        int j,n;
+
+        Console.WriteLine("Please type in 1st number");
+        Console.ReadLine();
+        Console.WriteLine("Please type in 2nd number");
+        Console.ReadLine();
+
+        Console.Write("Number ";
+        n=Convert.ToInt32(Console.ReadLine());
+
+        for(j=1<=10;j++)
+        {
+            Console.Write("{0}x{1}={2}\n",n,j,n*j);
+            
+        }
         }
     }
 }
